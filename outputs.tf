bf3640609f94408ab64568c4be082fa69c71c9fd
Sagar Jain <sagarjain1991@gmail.com>
2022-08-18 00:30:06 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -3,8 +3,4 @@ output "id" {
   description = "an identifier for the Cloud Scheduler job resource with format projects/{{project}}/locations/{{region}}/jobs/{{name}}"
 }
 
-output "id1" {
-  value       = module.hello_world_pipeline.id
-  description = "an identifier for the Cloud Scheduler job resource with format projects/{{project}}/locations/{{region}}/jobs/{{name}}"
-}
 
